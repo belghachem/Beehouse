e72bfb3c2dabb9b4415a06d1d6b249bf05fbaba4
belghachem <149454757+belghachem@users.noreply.github.com>
2025-12-28 16:44:49 +0100

    Update settings.py

--- BeeHouse/settings.py
+++ BeeHouse/settings.py
@@ -178,5 +178,5 @@ CLOUDINARY_STORAGE = {
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
