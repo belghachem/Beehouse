2e02327e7dde367fed646aa2510d7a06e7f251ca
belghachem <149454757+belghachem@users.noreply.github.com>
2025-12-28 18:25:20 +0100

    Update settings.py

--- BeeHouse/settings.py
+++ BeeHouse/settings.py
@@ -174,10 +174,11 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
-cloudinary.config( 
-    'CLOUD_NAME' = os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY' = os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET' = os.environ.get('CLOUDINARY_API_SECRET'),
-    'SECURE' = os.environ.get('CLOUDINARY_SECURE'),
-)
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME' : os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY' : os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET' : os.environ.get('CLOUDINARY_API_SECRET'),
+    'SECURE' : os.environ.get('CLOUDINARY_SECURE'),
+}
+
 
