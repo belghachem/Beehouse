3818a37e6a5f98dceb0139824d5ed134901564bc
belghachem <149454757+belghachem@users.noreply.github.com>
2025-12-28 17:22:56 +0100

    Update settings.py

--- BeeHouse/settings.py
+++ BeeHouse/settings.py
@@ -170,13 +170,14 @@ LOGGING = {
     },
 
 }
-
 import cloudinary
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-    secure=True
-)
+    'SECURE':os.environ.get('CLOUDINARY_SECURE')
 }
+
+# Use Cloudinary for media files
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
