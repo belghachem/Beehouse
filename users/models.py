060de0755c40111bd1b4607350f350d1db8f1b41
belghachem <149454757+belghachem@users.noreply.github.com>
2025-12-30 23:20:07 +0100

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from products.models import Product
+from cloudinary.models import CloudinaryField
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,7 +9,7 @@ class UserProfile(models.Model):
     address = models.TextField(blank=True)
     city = models.CharField(max_length=100, blank=True)
     postal_code = models.CharField(max_length=20, blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    profile_picture =CloudinaryField('profile_picture',blank=True, null=True)
     
     def __str__(self):
         return f"{self.user.username}'s Profile"
@@ -34,4 +35,5 @@ class Wishlist(models.Model):
         ordering = ['-added_date']
     
     def __str__(self):
+
         return f"{self.user.username} - {self.product.name}"
