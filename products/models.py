06e70b1afa6d73c16d74f5588145d982c8ea3fe2
belghachem <149454757+belghachem@users.noreply.github.com>
2026-01-20 22:57:27 +0100

    Add view_count field to Product model
    Added view_count field to track product views.

--- products/models.py
+++ products/models.py
@@ -12,6 +12,7 @@ class Product(models.Model):
     picture =CloudinaryField('picture',blank=True, null=True)
     picture_2 = CloudinaryField('picture_2',blank=True, null=True)
     picture_3 =CloudinaryField('picture_3',blank=True, null=True)
+    view_count = models.IntegerField(default=0, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
     class Meta:
@@ -49,5 +50,6 @@ class Product(models.Model):
         )['total'] or 0
     
     get_total_orders.short_description = "Total Orders"
+
 
 
