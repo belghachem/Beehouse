565939ae4c80d35a4d4b21d393e09513bfc5a3ba
belghachem <149454757+belghachem@users.noreply.github.com>
2026-01-21 12:24:22 +0100

    Add makemigrations for 'home' app in build script

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 pip install -r requirements.txt
+python manage.py makemigrations home
 python manage.py makemigrations
 python manage.py collectstatic --no-input
 python manage.py migrate 
